fca04b3244937359fa1364313936f80251f7f8dc
Oscar Söderlund <odsod@users.noreply.github.com>
2022-09-12 15:06:32 +0200

    chore: add GoDoc for ChargePointType values

--- chargepointtype.go
+++ chargepointtype.go
@@ -3,6 +3,7 @@ package monta
 // ChargePointType is the type of charge point.
 type ChargePointType string
 
+// Known [ChargePointType] values.
 const (
 	ChargePointTypeAC ChargePointType = "ac"
 	ChargePointTypeDC ChargePointType = "dc"
