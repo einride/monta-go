79304ad327040e94d5ff442d650be77dad190e22
Oscar Söderlund <odsod@users.noreply.github.com>
2022-09-13 15:46:47 +0200

    feat: add support for listing charges

---
+++ team.go
@@ -0,0 +1,11 @@
+package monta
+
+// Team of Monta users.
+type Team struct {
+
+	// ID of the team.
+	ID int64 `json:"id"`
+
+	// Public name of the team.
+	PublicName string `json:"publicName"`
+}
