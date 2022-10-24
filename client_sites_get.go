197483e4111a7d7196d9e2de09ecc0c152693111
Jusabe Guedes <jusabe.guedes@einride.tech>
2022-10-24 14:48:54 +0200

    chore: clean up code

--- client_sites_get.go
+++ client_sites_get.go
@@ -7,7 +7,7 @@ import (
 )
 
 // GetSite to retrieve a single (charge) site
-func (c *Client) GetSite(ctx context.Context, siteID int64) (_ *Site, err error) {
+func (c *Client) GetSite(ctx context.Context, siteID int64) (*Site, error) {
 	path := fmt.Sprintf("/v1/sites/%d", siteID)
 	return doGet[Site](ctx, c, path, url.Values{})
 }
