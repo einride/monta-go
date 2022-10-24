75c3c701e0c158d566cc17856b645e063d9ca504
Jusabe Guedes <jusabe.guedes@einride.tech>
2022-10-24 14:09:55 +0200

    refactor: avoid repetition

--- client_chargepoints_get.go
+++ client_chargepoints_get.go
@@ -2,44 +2,10 @@ package monta
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"net/url"
 )
 
 // GetChargePoint to retrieve a single charge point
 func (c *Client) GetChargePoint(ctx context.Context, chargePointID int64) (_ *ChargePoint, err error) {
-	method, path := http.MethodGet, fmt.Sprintf("/v1/charge-points/%d", chargePointID)
-	defer func() {
-		if err != nil {
-			err = fmt.Errorf("%s %s: %w", method, path, err)
-		}
-	}()
-	requestURL, err := url.Parse(apiHost + path)
-	if err != nil {
-		return nil, err
-	}
-	httpRequest, err := http.NewRequestWithContext(ctx, method, requestURL.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-	if err := c.setAuthorization(ctx, httpRequest); err != nil {
-		return nil, err
-	}
-	httpResponse, err := c.httpClient.Do(httpRequest)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		_ = httpResponse.Body.Close()
-	}()
-	if httpResponse.StatusCode != http.StatusOK {
-		return nil, newStatusError(httpResponse)
-	}
-	var response ChargePoint
-	if err := json.NewDecoder(httpResponse.Body).Decode(&response); err != nil {
-		return nil, err
-	}
-	return &response, nil
+	return getEntity[ChargePoint](ctx, c, fmt.Sprintf("/v1/charge-points/%d", chargePointID))
 }
--- client_sites_get.go
+++ client_sites_get.go
@@ -2,44 +2,10 @@ package monta
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"net/url"
 )
 
 // GetSite to retrieve a single (charge) site
 func (c *Client) GetSite(ctx context.Context, siteID int64) (_ *Site, err error) {
-	method, path := http.MethodGet, fmt.Sprintf("/v1/sites/%d", siteID)
-	defer func() {
-		if err != nil {
-			err = fmt.Errorf("%s %s: %w", method, path, err)
-		}
-	}()
-	requestURL, err := url.Parse(apiHost + path)
-	if err != nil {
-		return nil, err
-	}
-	httpRequest, err := http.NewRequestWithContext(ctx, method, requestURL.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-	if err := c.setAuthorization(ctx, httpRequest); err != nil {
-		return nil, err
-	}
-	httpResponse, err := c.httpClient.Do(httpRequest)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		_ = httpResponse.Body.Close()
-	}()
-	if httpResponse.StatusCode != http.StatusOK {
-		return nil, newStatusError(httpResponse)
-	}
-	var response Site
-	if err := json.NewDecoder(httpResponse.Body).Decode(&response); err != nil {
-		return nil, err
-	}
-	return &response, nil
+	return getEntity[Site](ctx, c, fmt.Sprintf("/v1/sites/%d", siteID))
 }
