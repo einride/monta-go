9ef98a83d0ca637b2372c7d138b3b436ca07261b
Jusabe Guedes <jusabe.guedes@einride.tech>
2022-10-24 14:50:41 +0200

    chore: finish descriptions with a dot

--- client_chargepoints_get.go
+++ client_chargepoints_get.go
@@ -6,7 +6,7 @@ import (
 	"net/url"
 )
 
-// GetChargePoint to retrieve a single charge point
+// GetChargePoint to retrieve a single charge point.
 func (c *Client) GetChargePoint(ctx context.Context, chargePointID int64) (*ChargePoint, error) {
 	path := fmt.Sprintf("/v1/charge-points/%d", chargePointID)
 	return doGet[ChargePoint](ctx, c, path, url.Values{})
--- client_charges_get.go
+++ client_charges_get.go
@@ -6,7 +6,7 @@ import (
 	"net/url"
 )
 
-// GetCharge to retrieve a single charge
+// GetCharge to retrieve a single charge.
 func (c *Client) GetCharge(ctx context.Context, chargeID int64) (*Charge, error) {
 	path := fmt.Sprintf("/v1/charges/%d", chargeID)
 	return doGet[Charge](ctx, c, path, url.Values{})
