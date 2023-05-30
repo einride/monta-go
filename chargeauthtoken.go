33aa11eacfbee80688a9fb75cd39399b18e22315
Jusabe Guedes <jusabe.guedes@einride.tech>
2023-05-30 13:06:38 +0200

    feat: add create charge auth token endpoint

--- chargeauthtoken.go
+++ chargeauthtoken.go
@@ -2,6 +2,15 @@ package monta
 
 import "time"
 
+// ChargeAuthTokenType is the types charge authentications.
+type ChargeAuthTokenType string
+
+// Known [ChargeAuthTokenType] values.
+const (
+	ChargeAuthTokenTypeVehicleID ChargeAuthTokenType = "vehicleId"
+	ChargeAuthTokenTypeRFID      ChargeAuthTokenType = "rfid"
+)
+
 // ChargeAuthToken used to authenticate the charge.
 type ChargeAuthToken struct {
 	// The id of the charge auth token.
@@ -9,7 +18,7 @@ type ChargeAuthToken struct {
 	// The identifier of the charge auth token, Note: without prefix e.g VID:.
 	Identifier string `json:"identifier"`
 	// The method type used for this charge auth token.
-	Type ChargeAuthType `json:"type"`
+	Type ChargeAuthTokenType `json:"type"`
 	// Id of the team that the charge auth token belongs.
 	TeamID int64 `json:"teamId"`
 	// Name of the charge auth token.
--- client_chargeauthtokens.go
+++ client_chargeauthtokens.go
@@ -1,11 +1,34 @@
 package monta
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"strconv"
+	"time"
 )
+
+// CreateChargeAuthTokenRequest is the request input to the [Client.CreateChargeAuthToken] method.
+type CreateChargeAuthTokenRequest struct {
+	// Id of the team the charge auth token belongs.
+	TeamID int64 `json:"teamId"`
+	// Id of the user the charge auth token should be associated to.
+	UserID *int64 `json:"userId"`
+	// Identifier of the the charge auth token. Note: without prefix e.g VID:
+	Identifier string `json:"identifier"`
+	// Type of the charge auth token.
+	Type ChargeAuthTokenType `json:"type"`
+	// Name of the charge auth token.
+	Name *string `json:"name"`
+	// If the charge auth token should be active in the Monta network.
+	MontaNetwork bool `json:"montaNetwork"`
+	// If the charge auth token should be active in the Roaming network.
+	RoamingNetwork bool `json:"roamingNetwork"`
+	// Until when the charge auth token should be active, when null it will be active forever.
+	ActiveUntil *time.Time `json:"activeUntil"`
+}
 
 // ListChargeAuthTokensRequest is the request input to the [Client.ListChargeAuthTokens] method.
 type ListChargeAuthTokensRequest struct {
@@ -20,6 +43,19 @@ type ListChargeAuthTokensResponse struct {
 	ChargeAuthTokens []*ChargeAuthToken `json:"data"`
 	// PageMeta with metadata about the current page.
 	PageMeta PageMeta `json:"meta"`
+}
+
+// CreateChargeAuthToken to create a new charge auth token.
+func (c *clientImpl) CreateChargeAuthToken(
+	ctx context.Context,
+	request CreateChargeAuthTokenRequest,
+) (*ChargeAuthToken, error) {
+	path := "/v1/charge-auth-tokens"
+	var requestBody bytes.Buffer
+	if err := json.NewEncoder(&requestBody).Encode(&request); err != nil {
+		return nil, err
+	}
+	return doPost[ChargeAuthToken](ctx, c, path, &requestBody)
 }
 
 // ListChargeAuthTokens to retrieve your charge auth tokens.
