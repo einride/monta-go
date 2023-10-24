5082849f2707d1dc72ecbdf7d06bf94de2a64737
Vitor da Silva <vitor.da-silva@einride.tech>
2023-10-24 22:22:52 +0200

    chore: simplify webhook requests and responses with WebhookConfig type

--- client_webhooks.go
+++ client_webhooks.go
@@ -9,24 +9,8 @@ import (
 
 const webhooksConfigBasePath = "/v1/webhooks/config"
 
-// GetWebhookConfigResponse is the response output from the [Client.GetWebhookConfig] method.
-type GetWebhookConfigResponse struct {
-	// A HTTPS URL to send the webhook payload to when an event occurs.
-	WebhookURL string `json:"webhookUrl"`
-	// A cryptoghrapic secret used to sign the webhook payload.
-	WebhookSecret string `json:"webhookSecret"`
-	// A list of event types to subscribe to. Use of ["*"] means subscribe to all.
-	EventTypes []*WebhookEventType `json:"eventTypes"`
-}
-
-// GetWebhookConfig to get your webhook config.
-func (c *clientImpl) GetWebhookConfig(ctx context.Context) (*GetWebhookConfigResponse, error) {
-	query := url.Values{}
-	return doGet[GetWebhookConfigResponse](ctx, c, webhooksConfigBasePath, query)
-}
-
-// UpdateWebhookConfigRequest is the request input to the [Client.UpdateWebhookConfig] method.
-type UpdateWebhookConfigRequest struct {
+// WebhookConfig is the request for and response from Get and Update WebhookConfig methods.
+type WebhookConfig struct {
 	// A HTTPS URL to send the webhook payload to when an event occurs.
 	WebhookURL string `json:"webhookUrl"`
 	// A cryptoghrapic secret used to sign the webhook payload.
@@ -35,26 +19,22 @@ type UpdateWebhookConfigRequest struct {
 	EventTypes []*WebhookEventType `json:"eventTypes"`
 }
 
-// UpdateWebhookConfigResponse is the response output from the [Client.UpdateWebhookConfig] method.
-type UpdateWebhookConfigResponse struct {
-	// A HTTPS URL to send the webhook payload to when an event occurs.
-	WebhookURL string `json:"webhookUrl"`
-	// A cryptoghrapic secret used to sign the webhook payload.
-	WebhookSecret string `json:"webhookSecret"`
-	// A list of event type to subscribe to. Use of ["*"] means subscribe to all.
-	EventTypes []*string `json:"eventTypes"`
+// GetWebhookConfig to get your webhook config.
+func (c *clientImpl) GetWebhookConfig(ctx context.Context) (*WebhookConfig, error) {
+	query := url.Values{}
+	return doGet[WebhookConfig](ctx, c, webhooksConfigBasePath, query)
 }
 
 // UpdateWebhookConfig to update your webhook config.
 func (c *clientImpl) UpdateWebhookConfig(
 	ctx context.Context,
-	request *UpdateWebhookConfigRequest,
-) (*UpdateWebhookConfigResponse, error) {
+	request *WebhookConfig,
+) (*WebhookConfig, error) {
 	var requestBody bytes.Buffer
 	if err := json.NewEncoder(&requestBody).Encode(&request); err != nil {
 		return nil, err
 	}
-	return doPut[UpdateWebhookConfigResponse](ctx, c, webhooksConfigBasePath, &requestBody)
+	return doPut[WebhookConfig](ctx, c, webhooksConfigBasePath, &requestBody)
 }
 
 // DeleteWebhookConfig to delete a webhook config.
