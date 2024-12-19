80746f40c354bef23e92fc44eedc18e458ebbf5c
Vitor da Silva <vitor.da-silva@einride.tech>
2024-12-19 18:17:55 +0200

    feat(CCO-1441): add price group endpoints

---
+++ client_pricegroups.go
@@ -0,0 +1,163 @@
+package monta
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+const priceGroupBasePath = "/v1/price-groups"
+
+// CreatePriceGroupRequest is the request input to the [Client.CreatePriceGroup] method.
+type CreateOrUpdatePriceGroupRequest struct {
+	// Team ID.
+	TeamID int64 `json:"teamId"`
+	// Name of the price group.
+	Name string `json:"name"`
+	// Type of the price group.
+	Type PriceGroupType `json:"type"`
+	// The master price.
+	MasterPrice Pricing `json:"masterPrice"`
+	// All the fees for the price group.
+	Fees *[]Pricing `json:"fees"`
+}
+
+// ListPriceGroupsRequest is the request input to the [Client.ListPriceGroups] method.
+type ListPriceGroupsRequest struct {
+	PageFilters
+	// Filter to retrieve price groups with specified team ID.
+	TeamID *int64
+}
+
+// ListPriceGroupsResponse is the response output from the [Client.ListPriceGroups] method.
+type ListPriceGroupsResponse struct {
+	// Price groups in the current page.
+	PriceGroups []*PriceGroup `json:"data"`
+	// PageMeta with metadata about the current page.
+	PageMeta PageMeta `json:"meta"`
+}
+
+// ApplyPriceGroupRequest is the request input to the [Client.ApplyPriceGroup] method.
+type ApplyPriceGroupRequest struct {
+	// List of charge point IDs.
+	SelectedChargePointIDs *[]int64 `json:"selectedChargePointIds"`
+	// List of site IDs.
+	SelectedSiteIDs *[]int64 `json:"selectedSiteIds"`
+	// List of team member IDs.
+	SelectedTeamMemberIDs *[]int64 `json:"selectedTeamMemberIds"`
+	// Flag to indicate if costs should be reset.
+	ResetCost *bool `json:"resetCost"`
+}
+
+// A pricing object for a create or update request body.
+type PricingRequestBody struct {
+	// Name of the pricing. It will be null when it's the master price.
+	Description *string `json:"description"`
+	// Type of the pricing. minute is used for Minute fee. min is used for the master price.
+	Type PricingType `json:"type"`
+	// Used by the Minute fee. True means it will stop charging the fee when the charge is complete.
+	// False means it will stop charging the fee when the cable is unplugged.
+	EndAtFullyCharged bool `json:"endAtFullyCharged"`
+	// Used by Spot Price. True means it will add % of VAT on top the price calculations.
+	// Note: vat rates differ from country to country.
+	VAT bool `json:"vat"`
+	// Used by Spot Price. It will multiply the fallback price by this percentage.
+	Percentage *float64 `json:"percentage"`
+	// The id of the selected Tariff
+	TariffID *int64 `json:"tariffId"`
+	// The id of the charge pricing tag for this pricing.
+	TagID *int64 `json:"tagId"`
+	// Used by Charging, Minute and Idle Fees. After how many minutes the fee should start being applied.
+	ApplyAfterMinutes *int32 `json:"applyAfterMinutes"`
+	// The price of this Fee or Master price.
+	Price Price `json:"price"`
+	// Used by spot price. The minimum that the raw spot price can be.
+	// This will be used in calculations if spot price is lower than this.
+	PriceMin *Price `json:"priceMin"`
+	// Used by spot price. The maximum that the raw spot price can be.
+	// This will be used in calculations if spot price is higher than this.
+	PriceMax *Price `json:"priceMax"`
+	// Used by Idle fee. The maximum the user will be charged for the idle fee.
+	FeePriceMax *Price `json:"feePriceMax"`
+	// Used by spot price. Additional absolute money or percentages values to be added on top of the previous calculations.
+	Additional *Additional `json:"additional"`
+	// DateTime "from" time to which this pricing should apply from.
+	From *time.Time `json:"from"`
+	// DateTime "to" time to which this pricing should apply to
+	To *time.Time `json:"to"`
+}
+
+// ListPriceGroups to retrieve your price groups.
+func (c *clientImpl) ListPriceGroups(
+	ctx context.Context,
+	request *ListPriceGroupsRequest,
+) (*ListPriceGroupsResponse, error) {
+	query := url.Values{}
+	request.PageFilters.Apply(query)
+	if request.TeamID != nil {
+		query.Set("teamId", strconv.Itoa(int(*request.TeamID)))
+	}
+	return doGet[ListPriceGroupsResponse](ctx, c, priceGroupBasePath, query)
+}
+
+// CreatePriceGroup to create a new price group.
+func (c *clientImpl) CreatePriceGroup(
+	ctx context.Context,
+	request CreateOrUpdatePriceGroupRequest,
+) (*PriceGroup, error) {
+	var requestBody bytes.Buffer
+	if err := json.NewEncoder(&requestBody).Encode(&request); err != nil {
+		return nil, err
+	}
+	return doPost[PriceGroup](ctx, c, priceGroupBasePath, &requestBody)
+}
+
+// GetPriceGroup to retrieve a single price group.
+func (c *clientImpl) GetPriceGroup(ctx context.Context, priceGroupID int64) (*PriceGroup, error) {
+	path := fmt.Sprintf("%s/%d", priceGroupBasePath, priceGroupID)
+	return doGet[PriceGroup](ctx, c, path, nil)
+}
+
+// UpdatePriceGroup to update a price group.
+func (c *clientImpl) UpdatePriceGroup(
+	ctx context.Context,
+	priceGroupID int64,
+	request CreateOrUpdatePriceGroupRequest,
+) (*PriceGroup, error) {
+	path := fmt.Sprintf("%s/%d", priceGroupBasePath, priceGroupID)
+	var requestBody bytes.Buffer
+	if err := json.NewEncoder(&requestBody).Encode(&request); err != nil {
+		return nil, err
+	}
+	return doPut[PriceGroup](ctx, c, path, &requestBody)
+}
+
+// DeletePriceGroup to delete a price group.
+func (c *clientImpl) DeletePriceGroup(ctx context.Context, priceGroupID int64) error {
+	path := fmt.Sprintf("%s/%d", priceGroupBasePath, priceGroupID)
+	return doDelete(ctx, c, path)
+}
+
+// ApplyPriceGroup to aply a price group to charge points, sites or team members.
+func (c *clientImpl) ApplyPriceGroup(
+	ctx context.Context,
+	priceGroupID int64,
+	request ApplyPriceGroupRequest,
+) (*PriceGroup, error) {
+	path := fmt.Sprintf("%s/%d/apply", priceGroupBasePath, priceGroupID)
+	var requestBody bytes.Buffer
+	if err := json.NewEncoder(&requestBody).Encode(&request); err != nil {
+		return nil, err
+	}
+	return doPut[PriceGroup](ctx, c, path, &requestBody)
+}
+
+// SetDefaultPriceGroup to set a price group as default.
+func (c *clientImpl) SetDefaultPriceGroup(ctx context.Context, priceGroupID int64) error {
+	path := fmt.Sprintf("%s/%d/default", priceGroupBasePath, priceGroupID)
+	return doPutNoBody(ctx, c, path)
+}
---
+++ pricegroup.go
@@ -0,0 +1,194 @@
+package monta
+
+import "time"
+
+// PriceGroup is a price group.
+type PriceGroup struct {
+	// ID of the price group.
+	ID int64 `json:"id"`
+
+	// Name of the price group.
+	Name string `json:"name"`
+
+	// Default price group.
+	Default bool `json:"default"`
+
+	// Type of the price group.
+	Type PriceGroupType `json:"type"`
+
+	// The master price.
+	MasterPrice Pricing `json:"masterPrice"`
+
+	// Tariffs of the price group.
+	Tariffs []Pricing `json:"tariffs"`
+
+	// Fees of the price group.
+	Fees []Pricing `json:"fees"`
+
+	// To how many team members the price group has been applied to.
+	TeamMemberCount *int32 `json:"teamMemberCount"`
+
+	// To how many charge points the price group has been applied to.
+	ChargePointCount *int32 `json:"chargePointCount"`
+
+	// Objects the price group is applied to.
+	AppliedTo *AppliedTo `json:"appliedTo"`
+
+	// When the price group was created.
+	CreatedAt time.Time `json:"createdAt"`
+
+	// When the price group was updated.
+	UpdatedAt *time.Time `json:"updatedAt"`
+}
+
+// Type of price group.
+type PriceGroupType string
+
+// Known [PriceGroupType] values.
+const (
+	PriceGroupTypePublic        PriceGroupType = "public"
+	PriceGroupTypeMember        PriceGroupType = "member"
+	PriceGroupTypeSponsored     PriceGroupType = "sponsored"
+	PriceGroupTypeCost          PriceGroupType = "cost"
+	PriceGroupTypeRoaming       PriceGroupType = "roaming"
+	PriceGroupTypeReimbursement PriceGroupType = "reimbursement"
+	PriceGroupTypeOther         PriceGroupType = "other"
+)
+
+// A pricing object.
+type Pricing struct {
+	// Id of the pricing.
+	ID int64 `json:"id"`
+
+	// Name of the pricing. It will be null when it's the master price.
+	Description *string `json:"description"`
+
+	// Type of the pricing. minute is used for Minute fee. min is used for the master price.
+	Type PricingType `json:"type"`
+
+	// If this is the master price (not a fee).
+	Master bool `json:"master"`
+
+	// If it's a dynamic price. It will be true if a tariffId is present.
+	DynamicPricing bool `json:"dynamicPricing"`
+
+	// Used by the Minute fee. True means it will stop charging the fee when the charge is complete.
+	// False means it will stop charging the fee when the cable is unplugged.
+	EndAtFullyCharged bool `json:"endAtFullyCharged"`
+
+	// Used by Spot Price. True means it will add % of VAT on top the price calculations.
+	// Note: vat rates differ from country to country.
+	VAT bool `json:"vat"`
+
+	// Used by Spot Price. It will multiply the fallback price by this percentage.
+	Percentage *float64 `json:"percentage"`
+
+	// The id of the selected Tariff
+	TariffID *int64 `json:"tariffId"`
+
+	// When the pricing was last updated.
+	UpdatedAt time.Time `json:"updatedAt"`
+
+	// Used by Charging, Minute and Idle Fees. After how many minutes the fee should start being applied.
+	ApplyAfterMinutes *int32 `json:"applyAfterMinutes"`
+
+	// The price of this Fee or Master price.
+	Price Price `json:"price"`
+
+	// Used by spot price. The minimum that the raw spot price can be.
+	// This will be used in calculations if spot price is lower than this.
+	PriceMin *Price `json:"priceMin"`
+
+	// Used by spot price. The maximum that the raw spot price can be.
+	// This will be used in calculations if spot price is higher than this.
+	PriceMax *Price `json:"priceMax"`
+
+	// Used by Idle fee. The maximum the user will be charged for the idle fee.
+	FeePriceMax *Price `json:"feePriceMax"`
+
+	// Used by spot price. Additional absolute money or percentages values to be added on top of the previous calculations.
+	Additional *[]Additional `json:"additional"`
+
+	// DateTime "from" time to which this pricing should apply from.
+	From *time.Time `json:"from"`
+
+	// DateTime "to" time to which this pricing should apply to
+	To *time.Time `json:"to"`
+
+	// The id of the charge pricing tag for this pricing.
+	TagID *int64 `json:"tagId"`
+}
+
+// Type of pricing.
+type PricingType string
+
+// Know [PricingType] values.
+const (
+	PricingTypeKwh      PricingType = "kwh"
+	PricingTypeMin      PricingType = "min"
+	PricingTypeSpot     PricingType = "spot"
+	PricingTypeTariff   PricingType = "tariff"
+	PricingTypeStarting PricingType = "starting"
+	PricingTypeCharging PricingType = "charging"
+	PricingTypeIdle     PricingType = "idle"
+	PricingTypeMinute   PricingType = "minute"
+)
+
+// Price of a fee, tariff or a master price.
+type Price struct {
+	// The amount of money.
+	Amount int64 `json:"amount"`
+	// Currency.
+	Currency PriceCurrency `json:"currency"`
+	// Current user locale.
+	Locale string `json:"locale"`
+}
+
+// Currency object of a Price.
+type PriceCurrency struct {
+	// ID of the currency.
+	ID *int64 `json:"id"`
+
+	// Whether the currency is master or not, master meaning the default currency.
+	Master *bool `json:"master"`
+
+	// 3 characters identifier.
+	Identifier string `json:"identifier"`
+
+	// Name of the currency.
+	Name *string `json:"name"`
+
+	// How many decimals the currency has.
+	Decimals int32 `json:"decimals"`
+}
+
+// An additional pricing.
+type Additional struct {
+	// Type of the additional pricing. absolute means the value is a price.
+	Type AdditionalType `json:"type"`
+	// The value of this additional pricing. Both absolute and percentage values here are represented as a money object.
+	Value Price `json:"value"`
+	// A title for this additional pricing.
+	Title *string `json:"title"`
+}
+
+// Type of an additional pricing.
+type AdditionalType string
+
+// Know [AdditionalType] values.
+const (
+	AdditionalTypeAbsolute   AdditionalType = "absolute"
+	AdditionalTypePercentage AdditionalType = "percentage"
+)
+
+// Objects a price group is applied to.
+type AppliedTo struct {
+	// ID's of the ChargePoints this price group has been applied to.
+	ChargePoints []int64 `json:"chargePoints"`
+
+	// ID's of the Sites this price group has been applied to.
+	Sites []int64 `json:"sites"`
+
+	// ID's of the Team Members this price group has been applied to.
+	TeamMembers []int64 `json:"teamMembers"`
+}
