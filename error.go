1301a547cfac904ab3711be3d7a95f8d002a8cd4
Andrei Iarysh <andrei.iarysh@einride.tech>
2025-04-17 15:37:06 +0200

    feat: add body field to status error

--- error.go
+++ error.go
@@ -1,6 +1,8 @@
 package monta
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -8,16 +10,23 @@ import (
 type StatusError struct {
 	Status     string
 	StatusCode int
+	Body       string
 }
 
 func newStatusError(httpResponse *http.Response) error {
+	body, _ := io.ReadAll(httpResponse.Body)
+
 	return &StatusError{
 		Status:     httpResponse.Status,
 		StatusCode: httpResponse.StatusCode,
+		Body:       string(body),
 	}
 }
 
 // Error implements error.
 func (s *StatusError) Error() string {
+	if s.Body != "" {
+		return fmt.Sprintf("HTTP %d %s: %s", s.StatusCode, s.Status, s.Body)
+	}
 	return s.Status
 }
