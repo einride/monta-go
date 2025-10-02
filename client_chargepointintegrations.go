796e31748724756f9f24a0cba681ce4223074fd3
Hugo Sandelius <hugo.sandelius@einride.tech>
2025-10-02 15:36:25 +0200

    feat: add ListChargePointIntegrations

--- client_chargepointintegrations.go
+++ client_chargepointintegrations.go
@@ -3,6 +3,8 @@ package monta
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"strconv"
 )
 
 // GetChargePointIntegration to retrieve a charge point integration.
@@ -12,4 +14,36 @@ func (c *clientImpl) GetChargePointIntegration(
 ) (*ChargePointIntegration, error) {
 	path := fmt.Sprintf("/v1/charge-point-integrations/%d", chargePointIntegrationID)
 	return doGet[ChargePointIntegration](ctx, c, path, nil)
+}
+
+// ListChargePointIntegrationsRequest is the request output from the [Client.ListChargePointIntegrations] method.
+type ListChargePointIntegrationsRequest struct {
+	PageFilters
+	// Allows to filter list of charge points integrations by a charge point id
+	ChargePointID int64
+	// Includes deleted resources in the response
+	IncludeDeleted *bool
+}
+
+// ListChargePointIntegrationsResponse is the response output from the [Client.ListChargePointIntegrations] method.
+type ListChargePointIntegrationsResponse struct {
+	// List of charges point integrations that match the criteria.
+	ChargePointIntegrations []*ChargePointIntegration `json:"data"`
+	// PageMeta with metadata about the current page.
+	PageMeta PageMeta `json:"meta"`
+}
+
+// ListChargePoints to retrieve your charge point integrations.
+func (c *clientImpl) ListChargePointIntegrations(
+	ctx context.Context,
+	request *ListChargePointIntegrationsRequest,
+) (*ListChargePointIntegrationsResponse, error) {
+	path := "/v1/charge-point-integrations"
+	query := url.Values{}
+	request.PageFilters.Apply(query)
+	query.Set("chargePointId", strconv.Itoa(int(request.ChargePointID)))
+	if request.IncludeDeleted != nil {
+		query.Set("includeDeleted", strconv.FormatBool(*request.IncludeDeleted))
+	}
+	return doGet[ListChargePointIntegrationsResponse](ctx, c, path, query)
 }
